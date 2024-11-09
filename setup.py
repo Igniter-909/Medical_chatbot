ac6f5f944eddc8c756bfa49a519c11fc82d1acce
Roshan Kumar Sahu <igniter909505@gmail.com>
2024-11-09 20:06:02 +0530

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.1",
+    author="Igniter-909",
+    author_email="igniterofficial909505@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
